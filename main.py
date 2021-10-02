67868a4bfe03b53ed0e7004bc38321a560430262
Kyle <s.t.o.r.m.detective@gmail.com>
2021-10-02 17:00:48 -0500

    testing

--- main.py
+++ main.py
@@ -4,26 +4,76 @@ import pandas as pd
 import streamlit
 import os
 from pathlib import Path
+import numpy as np
+import pydeck as pdk
 
 import streamlit as st
 from PIL import Image
 
 file_dir = Path(os.path.dirname(os.path.abspath(__file__)))
+DATE_TIME = "date/time"
+DATA_URL = file_dir/"data.csv"
 
+@st.cache(persist=True)
+def load_data(DATA_URL, nrows=None):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis="columns", inplace=True)
+    try:
+        data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
+    except KeyError:
+        pass
+    return data
+
+data = load_data(DATA_URL)
+extra = load_data("/Users/mosaicchurchhtx/Desktop/IncidenceReporting/heatmap-data.csv")
+extra = extra.dropna()
+# CREATING FUNCTION FOR MAPS
+
+def map(data, lat, lon, zoom):
+    st.write(pdk.Deck(
+        map_style="mapbox://styles/mapbox/light-v9",
+        initial_view_state={
+            "latitude": float(lat),
+            "longitude": float(lon),
+            "zoom": zoom,
+            "pitch": 50,
+        },
+        layers=[
+            pdk.Layer(
+                "HexagonLayer",
+                data=data,
+                get_position=["lng", "lat"],
+                radius=100,
+                elevation_scale=50,
+                elevation_range=[0, 3000],
+                pickable=True,
+                extruded=True,
+                coverage=1,
+            ),
+        ]
+    ))
+
+zoom_level = 12
+midpoint = (np.average(extra['lat']), np.average(extra['lng']))
 
 
 def main():
-    img = Image.open(file_dir/"images"/"ConocoPhillips-Logo-Small.png")
+    img = Image.open(file_dir/"Images"/"ConocoPhillips-Logo-Small.png")
     st.image(img, width= 150,use_column_width=200)
 
-    img2 = Image.open(file_dir/"images"/"background.jpeg")
+    img2 = Image.open(file_dir/"Images"/"background.jpeg")
     st.image(img2, width= 1440,)
 
-    img2 = Image.open("/Users/CARLOSPARLOUR/Documents/Python/IncidenceReporting/Images/background.jpeg")
     st.image(img2, width=1260)
 
     #Title
     st.header("Incidents Reporting")
+
+    # test_data = pd.concat([data]*20, ignore_index=True)
+
+    # print(test_data[["lat", "lon"]])
+    map(extra, midpoint[0], midpoint[1], 11)
 
     with st.form("my_form"):
         st.write("Please Fill Out Information Below")
@@ -31,36 +81,5 @@ def main():
         submitted = st.form_submit_button("Submit")
 
 
-
-
-
-
-
-
 if __name__ == "__main__":
     main()
-
-
-# def simulate():
-#     img = Image.open("logo.jpg")
-#     st.image(img)
-#     print()
-#     # header_html = "<img src='data:image/png;base64,{}' class='img-fluid'>".formatimg_to_bytes("header.png")
-#     # st.markdown(header_html, unsafe_allow_html=True,)
-#
-#
-# def img_to_bytes(img_path):
-#     img_bytes = Path(img_path).read_bytes()
-#     encoded = base64.b64encode(img_bytes).decode()
-#     return encoded
-#
-# add_selectbox = st.sidebar.selectbox(
-#     'How would you like to be contacted?',
-#     ('Email', 'Home phone', 'Mobile phone')
-# )
-#
-# # Add a slider to the sidebar:
-# add_slider = st.sidebar.slider(
-#     'Select a range of values',
-#     0.0, 100.0, (25.0, 75.0)
-# )
