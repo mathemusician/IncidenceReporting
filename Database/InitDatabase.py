e45f24e713af6e3058bdfd1cb59ddcf779d87769
psudario <philllipsudario21@gmail.com>
2021-10-02 16:30:21 -0500

    refactored InitDatabase

--- Database/InitDatabase.py
+++ Database/InitDatabase.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 
 if os.environ.get('DB_NAME') == None:
-    db= mysql.connector.connect(
+    db = mysql.connector.connect(
         host    =   os.environ.get('HOST'),
         user    =   os.environ.get('DB_USERNAME'),
         passwd  =   os.environ.get('DB_PASSWORD')
@@ -19,7 +19,17 @@ if os.environ.get('DB_NAME') == None:
     dbcursor.execute("DROP DATABASE IncidentDatabase")
     dbcursor.execute("CREATE DATABASE IncidentDatabase")
     dbcursor.execute("USE IncidentDatabase")
-    dbcursor.execute("CREATE TABLE Incidents(IncidentID INT PRIMARY KEY AUTO_INCREMENT)")
+    dbcursor.execute("""CREATE TABLE Incidents(
+        IncidentID INT PRIMARY KEY AUTO_INCREMENT, 
+        IncidentDate DATETIME DEFAULT NOW(),
+        IncidentType VARCHAR(255),
+        IncidentLocation VARCHAR(255),
+        FileURI VARCHAR(255),
+        Description VARCHAR(1000)
+    )""")
     
     with open('.env', 'a') as env_file:
         env_file.write('DB_NAME=IncidentDatabase\nTABLE_NAME=Incidents\n')
+
+
+
